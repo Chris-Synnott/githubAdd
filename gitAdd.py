590ad02846e89f6559400b97beb028a8a9e9fe70
Alexander Behrmann <alexander.behrmann@hotmail.com>
2019-04-10 21:41:51 -0700

    first draft

--- gitAdd.py
+++ gitAdd.py
@@ -4,15 +4,16 @@ from subprocess import call
 class githubAdd:
 
     def __init__(self):
+        # You can modify githubUserList.csv to any .csv file of your choice. Remember to leave the first line blank
         self.readGithubUsersCSV('githubUserList.csv')
 
     def readGithubUsersCSV(self, inputFile):
         with open(inputFile) as csvfile:
             users = csv.reader(csvfile, delimiter=',')
             for row in users:
-                print (row)
-                
-            call('curl -X PUT -u "username":"password" https://api.github.com/orgs/fmavborg/memberships/'+row[0], shell=True)
+                # Replace username with your username, password with your password, and YOURORGNAMEHERE with your
+                # organisation name
+                call('curl -X PUT -u "username":"password" https://api.github.com/orgs/YOURORGNAMEHERE/memberships/' + row[0], shell=True)
 
         return users
         
