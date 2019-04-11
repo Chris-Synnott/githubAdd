7b434027c4582fed199153927186bc743e91ed67
Alexander Behrmann <alexander.behrmann@hotmail.com>
2019-04-10 19:32:16 -0700

    virtualenv added and test request added

--- gitAdd.py
+++ gitAdd.py
@@ -1,16 +1,19 @@
 import csv
+import requests
 
 class githubAdd:
 
     def __init__(self):
-        self.readGithubUsersCSV('githubUserList.csv');
+        self.readGithubUsersCSV('githubUserList.csv')
 
     def readGithubUsersCSV(self, inputFile):
         with open(inputFile) as csvfile:
             users = csv.reader(csvfile, delimiter=',')
             for row in users:
-                breakpoint()
-                print(', '.join(row))
+                #breakpoint()
+                URL = "https://api.github.com/orgs/YOURORGNAMEHERE/memberships/" + row[0]
+                r = requests.put(url=URL)
+                print (r.json())
 
 if __name__ == "__main__":
     #breakpoint()
